377e0d5381dc510bac92c755054257153e665824
genefschmidtjr <43154730+genefschmidtjr@users.noreply.github.com>
2018-09-11 18:27:16 -0400

    refined data seed module by adding main and function definitions across the board.  added main to the product dimension builder.  Added test file.  added postgres scripts

--- test_product_dimension_builder.py
+++ test_product_dimension_builder.py
@@ -3,6 +3,13 @@ import product_dimension_builder
 
 
 class TestProductDimensionBuilder(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
     """ simple test to make sure we are connecting through alchemy"""
     def test_connect_exists(self):
         con, meta = product_dimension_builder.connect('Gene','Gene', 'MyCompany')
